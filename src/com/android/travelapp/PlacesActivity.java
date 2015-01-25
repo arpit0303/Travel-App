fdf3b5e992dc21b3ae02efc5ff6fef9ac119f861
ARPIT AGRAWAL <arpitagrawal910@gmail.com>
2015-01-25 14:30:49 +0530

    set google map to my location

--- src/com/android/travelapp/PlacesActivity.java
+++ src/com/android/travelapp/PlacesActivity.java
@@ -1,9 +1,16 @@
 package com.android.travelapp;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +25,24 @@ public class PlacesActivity extends Activity {
 		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 		
 		map.setMyLocationEnabled(true);
+		
+		LocationManager locMngr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		
+		// Create a criteria object to retrieve provider 
+        Criteria criteria = new Criteria();
+        
+        // Get the name of the best provider 
+        String provider = locMngr.getBestProvider(criteria, true); 
+ 
+        // Get Current Location 
+        Location myLocation = locMngr.getLastKnownLocation(provider); 
+		
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+	    .target(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()))      // Sets the center of the map to Mountain View
+	    .zoom(12)                  // Sets the tilt of the camera to 30 degrees
+	    .build();  
+		
+		map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 	}
 
 	@Override
